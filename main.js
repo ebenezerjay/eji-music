cca3be6b5b58caccf800817fdf50576b93d9e646
Ebenezer Jay Ingalsbe <[ebenezerjaymusic@gmail.com]>
2020-01-17 16:08:40 -0700

    Add graphics and tour screen

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // global variables 
 const enterButton = $(".enter-button");
 const contentBoxSection = $(".contentBox-section");
-const popUpArticle = $("#popUp-article-id");
+const popUpArticle = $(".popUp-article");
+const divDisplay = $(".displayDiv");
 const div1 = $("#div1-id");
 const div2 = $("#div2-id");
 const div3 = $("#div3-id");
@@ -17,10 +18,7 @@ document.body.addEventListener('mousemove', function(e) {
 });
 
 $(document).ready(function() {
-	$(div1).css("display", "none");
-	$(div2).css("display", "none");
-	$(div3).css("display", "none");
-	$(div4).css("display", "none");
+	$(divDisplay).css("display", "none");
 });
 
 enterButton.on('click', splitIntoBoxes);
@@ -29,29 +27,58 @@ div2.on('click', musicSection);
 div3.on('click', epkSection);
 div4.on('click', bioSection);
 
+// changes display after enter button is clicked
+function displayTheDivs() {
+	$(divDisplay).css("display", "flex");
+}
 
 // changes background of content box into 4 separate clickable boxes
 function splitIntoBoxes() {
+	displayTheDivs();
 	$(contentBoxSection).css("backgroundImage", "url('images/EJI-options-screen.jpg')");
-	$(contentBoxSection).css("border", "none");
-	// contentBoxSection.style.backgroundImage = "url('images/EJI-options-screen.jpg')";
-	// contentBoxSection.style.border = "none";
 	$(enterButton).css("display", "none");
 }
 
-// changes background to the tour section
+// changes background to the tour/live shows content
 function tourSection() {
-	console.log(div1);
+	$(divDisplay).css("display", "none");
+	$(contentBoxSection).css("background", "black");
+	$(contentBoxSection).css("border", "none");
+	let tourImg = new Image(500,200);
+	tourImg.src = "images/tour-graphic.jpg";
+	$(popUpArticle).append(tourImg);
+	$(popUpArticle).append("<h2>Poor Bodhi- May 11th, Headroom Sessions @Recreative Denver</h2>");
 }
 
+// changes background to the music content
 function musicSection() {
-	console.log(div2);
+	$(divDisplay).css("display", "none");
+	$(contentBoxSection).css("background", "black");
+	let musicImg = new Image(500,200);
+	musicImg.src = "images/music-graphic.jpg";
+	$(popUpArticle).append(musicImg);
+	$(popUpArticle).append(<audio></audio>);
+	$("audio").attr({
+		class: 'audio-controls',
+		controls: "",
+		src: "",
+	})
 }
 
+// changes background to the epk content
 function epkSection() {
-	console.log(div3);
+	$(divDisplay).css("display", "none");
+	$(contentBoxSection).css("background", "black");	
+	let epkImg = new Image(500,200);
+	epkImg.src = "images/epk-graphic.jpg";
+	$(popUpArticle).append(epkImg);
 }
 
+// changes background to the bio/contact content
 function bioSection() {
-	console.log(div4);
+	$(divDisplay).css("display", "none");
+	$(contentBoxSection).css("background", "black");
+	let bioImg = new Image(500,200);
+	bioImg.src = "images/bio-graphic.jpg";
+	$(popUpArticle).append(bioImg);
 }
