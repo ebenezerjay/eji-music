87f7437357000a9425fdf8c6626c05e7a55453be
Ebenezer Jay Ingalsbe <[ebenezerjaymusic@gmail.com]>
2020-08-04 13:04:03 -0600

    Add news button

--- main.js
+++ main.js
@@ -1,63 +1,15 @@
-// global variables 
-const enterButton = $(".enter-button");
-const contentBoxSection = $(".contentBox-section");
-const popUpArticle = $(".popUp-article");
-const divDisplay = $(".displayDiv");
-const div1 = $("#div1-id");
-const div2 = $("#div2-id");
-const div3 = $("#div3-id");
-const div4 = $("#div4-id");
+// global variables
+const circleImgL = $('.circle-img-left');
 
 
-// event listeners
-
-
-// audio objects
- const soSadAudio = {
-	name: "So Sad(still)",
-	src: "audio/So-Sad-(still)-copy.mp3",
-}
-
-const mrReevesAudio = {
-	name: "Mr Reeves Class",
-	src: "audio/Mr-Reeves-Class-copy.mp3",
-}
-
-const danceableKitchenAudio = {
-	name: "Danceable Kitchen",
-	src: "audio/Danceable-Kitchen-copy.wav",
-}
-
-const insertLyicsAudio = {
-	name: "Insert Lyrics Here",
-	src: "audio/Insert-lyrics-here-copy.mp3",
-}
-
-// adds the attributes to each audio element added to dom
-function addAttributes() {
-	
-}
-
-// adds and styles so sad audio
-function soSad() {
-	$(popUpArticle).append("<audio></audio>");
-	let soSadSrc = soSadAudio.src;
-	$()
-}
-
-// adds and styles Mr Reeves Class audio
-function mrReevesClass() {
-	$(popUpArticle).append("<audio></audio>");
-	$("audio").attr({
-		class: 'audio-controls flex',
-		controls: "controls",
-		src: "audio/So-Sad-(still)-copy.mp3",
-	});
-}
-
-// functionality for hamburger drop down icon
+// show news/updates
 $(document).ready(function() {
-	$("#nav-ham-icon-id").on('click', function() {
-		$("#ul-site-links-id").toggleClass("ul-site-links");
+	$(circleImgL).on('click', function() {
+		let articleDivImgL = $('.article-div-images-left');
+		$(articleDivImgL).animate({
+			marginLeft: '100%'
+		});
+		$(articleDivImgL).fadeOut("slow","swing");
 	});
 });
+
